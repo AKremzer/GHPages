09f9a849ef55ff642f3711d6a21cba903da930b1
AKremzer <75171579+AKremzer@users.noreply.github.com>
2023-04-17 16:35:52 +0700

    Update game-script.js

--- game-script.js
+++ game-script.js
@@ -1,5 +1,27 @@
-const textarea = document.getElementById('your-code')
-textarea.addEventListener('input', () => {
-    textarea.style.height = 0;
-    textarea.style.height = (textarea.scrollHeight - 4) + "px";
+let user_input = document.getElementById('your-code')
+
+user_input.addEventListener('input', () => {
+    user_input.style.height = 0;
+    user_input.style.height = (user_input.scrollHeight + 1.5) + "px";
 })
+
+user_input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        let lines = user_input.value.split(/\r\n|\r|\n/);
+        if (lines.length >= parseInt(user_input.rows)) {
+            e.preventDefault();
+        }
+    }
+})
+
+user_input.addEventListener('input', () => {
+    let elementName = document.getElementById("my-code-1").innerHTML.split(" ")[0];
+    let elementToChange = null;
+    if (elementName[0] == '#') {
+        elementName = elementName.substring(1);
+        elementToChange = document.getElementById(elementName);
+        elementToChange.style.cssText = user_input.value;
+        console.log(user_input.value);
+    }
+})
+
